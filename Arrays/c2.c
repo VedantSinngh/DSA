f05121406a9a928c521399961d4c139ef4d14543
VedantSinngh <vedaantsinngh@gmail.com>
2025-01-14 12:11:19 +0530

    Arrays

---
+++ Arrays/c2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// using namespace std;
+
+int main() {
+    int A[5];
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("Enter a number: ");
+        scanf("%u", &A[i]);
+    }
+    return 0;
+}
